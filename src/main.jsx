5b7b5552f6858990ea196c8cd02f9eed3272c434
Sikha Babu <sikhababu27@gmail.com>
2025-02-04 22:50:54 +0530

    fourth commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,11 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Provider } from 'react-redux'
-import store from './App/Store.js';
+import Store from './App/Store';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <Provider store={store}>
+   <Provider store={Store}>
       <App />
     </Provider>
   </StrictMode>,
